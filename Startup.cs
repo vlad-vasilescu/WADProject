0cdfa2b5a34e6d8429666fa9d2bc1abccd57373e
metwadsprite <vladvas98@yahoo.com>
2020-03-01 15:05:58 +0200

    Add fontawesome.

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,7 @@ namespace WADProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
+                options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddRoles<IdentityRole>()
